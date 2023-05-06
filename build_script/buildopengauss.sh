8ba37f5b40967913f019b188b7e6a3c5a9d0d59a
jianghongbo <xdhbj0022@163.com>
2023-05-06 10:44:21 +0800

    add build script

---
+++ build_script/buildopengauss.sh
@@ -0,0 +1,293 @@
+
+#!/bin/bash
+# Copyright (c) Huawei Technologies Co., Ltd. 2010-2022. All rights reserved.
+# description: 
+# date: 2022-10-11
+
+set -e
+
+### copy this script under each platform and run to build
+docker_name=opengauss-build-001
+docker_image_name=opengauss-docker-build:v1
+#区分master分支和5.1.0分支
+branch_type=$2
+volume_dir=/data2/opengauss/volume_${branch_type}
+tracer_dir=/data2/opengauss/tracer
+root_dir=$(pwd)
+remote_test_host=192.168.0.245
+VERSION="5.1.0"
+os=""
+dockerfile="Dockerfile"
+obs_upload_path=obs://opengauss/latest
+
+
+function log() {
+    local level="$1"
+    local msg_cont="$2"
+    date_time=$(date +'%F %H:%M:%S')
+
+    log_format="${date_time} [${level^^}] funcname: ${FUNCNAME[1]} line:$(caller 0 | awk '{print$1}') ${msg_cont}"
+    case "${level}" in
+        debug)
+            echo -e "${log_format}"
+            ;;
+        info)
+            echo -e "${log_format}"
+            ;;
+        warn)
+            echo -e "${log_format}"
+            ;;
+        error)
+            echo -e "${log_format}"
+            ;;
+    esac
+}
+
+function gen_sha256()
+{
+    pkg_volume=$1
+    cd ${pkg_volume}
+    package_list=$(ls *.tar.gz | grep "openGauss" | xargs -n 1 basename)
+    for package_name in ${package_list[@]};
+    do
+        sha256sum -b $package_name > $package_name.sha256
+    done
+}
+
+function get_os(){
+    arch=$(uname -m)
+    if [ "X$arch" == "Xaarch64" ]; then
+        os="arm"
+        dockerfile=${dockerfile}-oelarm
+       if [[ "x${volume_dir}" =~ "x/data2" ]]; then
+           os=${os}_2203
+	   dockerfile=${dockerfile}_2203
+       fi
+    else
+        if [ -f "/etc/openEuler-release" ];then
+            os="x86_openEuler"
+	    dockerfile=${dockerfile}-oelx86
+            if [[ "x${volume_dir}" =~ "x/data2" ]]; then
+               os=${os}_2203
+	       dockerfile=${dockerfile}_2203
+            fi
+        else
+            os="x86"
+            dockerfile=${dockerfile}-x86
+        fi
+    fi
+
+    echo "${dockerfile}"
+
+}
+
+function uplod_package() {
+    if [ X"${branch_type}" == X"5.1.0" ]; then
+        obs_upload_path=${obs_upload_path}/5.1.0
+    fi
+
+    if [ $2 = "debug" ]; then
+            obs_upload_path=${obs_upload_path}/debug
+    fi
+    pkg_volume=$1
+    cd ${pkg_volume}
+    get_os
+    dbs_dest=${os}
+
+    kernel=""
+    version=""
+    ext_version=""
+    dist_version=""
+    if [ -f "/etc/euleros-release" ]; then
+        kernel=$(cat /etc/euleros-release | awk -F ' ' '{print $1}' | tr A-Z a-z)
+        version=$(cat /etc/euleros-release | awk -F '(' '{print $2}'| awk -F ')' '{print $1}' | tr A-Z a-z)
+        ext_version=$version
+    elif [ -f "/etc/openEuler-release" ]; then
+        kernel=$(cat /etc/openEuler-release | awk -F ' ' '{print $1}' | tr A-Z a-z)
+        version=$(cat /etc/openEuler-release | awk -F '(' '{print $2}'| awk -F ')' '{print $1}' | tr A-Z a-z)
+    elif [ -f "/etc/centos-release" ]; then
+        kernel=$(cat /etc/centos-release | awk -F ' ' '{print $1}' | tr A-Z a-z)
+        version=$(cat /etc/centos-release | awk -F '(' '{print $2}'| awk -F ')' '{print $1}' | tr A-Z a-z)
+    else
+        kernel=$(lsb_release -d | awk -F ' ' '{print $2}'| tr A-Z a-z)
+        version=$(lsb_release -r | awk -F ' ' '{print $2}')
+    fi
+
+    if [ X"$kernel" == X"euleros" ]; then
+        dist_version="EULER"
+    elif [ X"$kernel" == X"centos" ]; then
+        dist_version="CentOS"
+    elif [ X"$kernel" == X"openeuler" ]; then
+        dist_version="openEuler"
+    else
+        echo "Only support EulerOS|Centos|openEuler platform."
+        echo "Kernel is $kernel"
+        exit 1
+    fi
+
+    local upload_log=upload.log
+    local upload_1_log=upload_1.log
+    rm -rf ${upload_log}
+    rm -rf ${upload_1_log}
+
+    package_list=$(ls *.tar.gz | grep "openGauss" | xargs -n 1 basename)
+    package_num=$(ls *.tar.gz | grep "openGauss" | wc -l)
+    for package_name in ${package_list[@]};
+    do
+        sha256sum -b $package_name > $package_name.sha256
+        push_command1="/home/obsutil cp ${package_name} ${obs_upload_path}/${dbs_dest}/"
+        push_command2="/home/obsutil cp ${package_name}.sha256 ${obs_upload_path}/${dbs_dest}/"
+        set +e
+        $push_command1 | tee -a $upload_log
+        $push_command2 | tee -a $upload_log
+    done
+
+    # upload server for simpleinstall
+    local upload_2_log=upload_2.log
+    rm -rf ${upload_2_log}
+    server_list=$(ls ${pkg_volume} | grep "openGauss" |grep -E "*.bz2|64bit.sha256"| xargs -n 1 basename)
+    for ser_name in ${server_list[@]};
+    do
+        push_command1="/home/obsutil cp ${pkg_volume}/${ser_name} ${obs_upload_path}/${dbs_dest}/"
+        $push_command1 | tee -a $upload_2_log
+    done
+
+    push_command_git="/home/obsutil cp git_num.txt ${obs_upload_path}/${dbs_dest}/"
+    $push_command_git | tee -a $upload_1_log
+
+    set -e
+    if [ -f $upload_log ]; then
+        # 值为1表示编译成功
+        is_uload_package_success=$(grep "Upload successfully" $upload_log | wc -l)
+        if [ $is_uload_package_success -eq $((package_num*2)) ]; then
+            log 'info' 'upload package to obs successfully '
+        else
+            log 'error' 'upload package to obs failed'
+            exit 1
+        fi
+    else
+        log 'error' 'not generate upload.log upload package to obs failed'
+        exit 1
+    fi
+}
+
+# 构建容器镜像
+# 对于三方库有修改（临时，后面三方库一起编译）、构建脚本有修改的需要重新打镜像。
+function build_docker_image()
+{
+    docker build -t ${docker_image_name} -f ./${dockerfile} . 
+    if [ $? -ne 0 ]; then
+        echo "build opengauss-build image failed.."
+        exit 1
+    fi
+}
+
+function clean_docker_instance()
+{
+    # rm normal container
+    container_ids=$(docker ps | grep ${docker_name} | awk '{print $1}')
+    if [ "${container_ids}" != "" ]; then
+        docker kill ${container_ids} && docker rm ${container_ids}
+    fi
+
+    # rm abnormal container
+    abn_container_ids=$(docker ps -a | grep ${docker_name} | awk '{print $1}')
+    if [ "${abn_container_ids}" != "" ]; then
+        docker rm ${abn_container_ids}
+    fi
+
+    # rm none docker images
+    non_image_ids=$(docker images | grep none | awk '{print $3}')
+    if [ "${non_image_ids}" != "" ]; then
+        docker rmi -f ${non_image_ids}
+    fi
+
+    #rm exited container
+    exited_container_ids=$(docker ps -a|grep Exited|awk '{print $1}')
+    if [ "${exited_container_ids}" != "" ]; then
+	docker rm ${exited_container_ids}
+    fi
+}
+
+function send_pkg_to_test()
+{
+    arch="CentOS"
+    if [ ! -f /etc/centos-release ]; then
+        arch="openEuler"
+    fi
+    coretype=$(uname -p)
+
+    cd ${volume_dir}
+    package_list=$(ls | grep "openGauss")
+    for file in ${package_list[@]};
+    do
+        set +x
+        sshpass -p "paswswd" scp "$file" "root@${remote_test_host}:/data/packages/opengauss/"
+        set -x
+    done
+    
+    cd ${tracer_dir}
+    cd openGauss-${VERSION}-${arch}-${coretype}-Python.tar.gz
+    tracer_list=$(ls | grep *tar.gz)
+    for file in ${tracer_list[@]};
+    do
+        set +x
+        sshpass -p "paswswd" scp "$file" "root@${remote_test_host}:/data/packages/opengauss/"
+        set -x
+    done
+
+    cd ../openGauss-${VERSION}-JDBC.tar.gz
+    tracer_list=$(ls | grep *tar.gz)
+    for file in ${tracer_list[@]};
+    do
+        set +x
+        sshpass -p "paswswd" scp "$file" "root@${remote_test_host}:/data/packages/opengauss/"
+        set -x
+    done
+}
+
+
+function trigger_remote_upload()
+{
+    sshpass -p "paswswd" ssh root@${remote_test_host} "cd /data/packages; sh upload.sh /data/packages/opengauss"
+}
+
+
+
+function main()
+{
+    if [ $1 = "debug" ]; then
+        pkg_type=debug
+        volume_dir=${volume_dir}_debug
+    else
+        pkg_type=release
+    fi
+
+    if [ -d "${volume_dir}" ]; then
+        rm -rf ${volume_dir}
+    fi
+    mkdir -p ${volume_dir}
+
+    get_os
+
+    clean_docker_instance
+    build_docker_image
+
+    containerid=`docker run --privileged=true -d -it -P -v ${volume_dir}:/usr1/build/workspace/volume -v /etc/localtime:/etc/localtime -v ${tracer_dir}:/usr1/build/workspace/tracer/ -e PKG_TYPE=${pkg_type}  --name ${docker_name} ${docker_image_name} $branch_type`
+    docker logs -f ${containerid}
+
+    echo "build opengauss finished......"
+    echo "show build files......"
+    ls -l ${volume_dir}
+
+    if [ $(ls ${volume_dir} |grep openGauss |wc -l) -gt 0 ]; then
+	    uplod_package ${volume_dir} ${pkg_type}
+	    cd $root_dir
+	    if [ X"${branch_type}" == X"master" ]; then
+                sh storeTestVersion.sh ${volume_dir} ${pkg_type} ${branch_type}
+            fi
+    fi
+}
+
+main $@
+
---
+++ build_script/entrypoint.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+
+yum install -y bzip2 bzip2-devel curl libaio libaio-devel flex bison ncurses-devel glibc-devel patch dkms readline-devel \
+which git libtool-ltdl-devel autoconf libtool python3 python3-devel python openssl-devel gcc-c++ make hostname iproute dos2unix
+
+# build type
+export WORKSPACE=/usr1/build/workspace
+export PKG_PRE_FIX=openGauss
+export PKG_VERSION=5.1.0
+export TARGET_OS=openEuler
+export OUT_PUT_PATH=/usr1/build/workspace/result/
+export VOLUME_PATH=/usr1/build/workspace/volume
+
+#docker run传递参数
+repo_branch=$1
+# source and branch
+server_repo=https://gitee.com/opengauss/openGauss-server.git
+server_branch=master
+om_repo=https://gitee.com/opengauss/openGauss-OM.git
+om_branch=master
+jdbc_repo=https://gitee.com/opengauss/openGauss-connector-jdbc.git
+jdbc_branch=master
+odbc_repo=https://gitee.com/opengauss/openGauss-connector-odbc.git
+odbc_branch=master
+python_repo=https://gitee.com/opengauss/openGauss-connector-python-psycopg2.git
+python_branch=master
+cm_repo=https://gitee.com/opengauss/CM.git
+cm_branch=master
+dcf_repo=https://gitee.com/opengauss/DCF.git
+dcf_branch=master
+cbb_repo=https://gitee.com/opengauss/CBB.git
+cbb_branch=master
+dcc_repo=https://gitee.com/opengauss/DCC.git
+dcc_branch=master
+plugin_repo=https://gitee.com/opengauss/Plugin.git
+plugin_branch=master
+
+dss_repo=https://gitee.com/opengauss/DSS.git
+dss_branch=master
+dms_repo=https://gitee.com/opengauss/DMS.git
+dms_branch=master
+
+cm_restapi_repo=https://gitee.com/opengauss/CM-RestAPI.git
+cm_restapi_branch=master
+
+opengauss_source=${WORKSPACE}/openGauss
+
+function download_from_gitee() {
+    repo=$1
+    branch=$2
+    target_dir=$3
+    echo "download source [${repo}], branch [${branch}]"
+
+    git config --global core.compression 0
+    a=0
+    flag=0
+    while [ $a -lt 3 ]
+    do
+    echo $a
+    rm -rf ${WORKSPACE}/OM
+    timeout 120 git clone ${repo} -b ${branch} ${target_dir} > /dev/null
+    if [[ $? == 0 ]] ; then
+        flag=1
+        break
+    fi
+    a=`expr $a + 1`
+    done
+
+    if [[ $flag == 0 ]] ; then
+        echo "clone code failed!"
+        exit 1
+    fi
+}
+
+function download_source() {
+    cd ${opengauss_source}
+    download_from_gitee ${server_repo} ${repo_branch} server
+    download_from_gitee ${om_repo} ${repo_branch} OM
+    download_from_gitee ${jdbc_repo} ${repo_branch} jdbc
+    download_from_gitee ${odbc_repo} ${repo_branch} odbc
+    download_from_gitee ${python_repo} ${repo_branch} python_driver2
+    download_from_gitee ${cm_repo} ${repo_branch} CM
+    download_from_gitee ${dcf_repo} ${repo_branch} DCF
+    download_from_gitee ${cbb_repo} ${repo_branch} CBB
+    download_from_gitee ${dcc_repo} ${repo_branch} DCC
+    download_from_gitee ${server_repo} ${repo_branch} server_lite
+    download_from_gitee ${plugin_repo} ${repo_branch} plugins
+    download_from_gitee ${cm_restapi_repo} ${repo_branch} cm_restapi
+    download_from_gitee ${dss_repo} ${repo_branch} DSS
+    download_from_gitee ${dms_repo} ${repo_branch} DMS
+}
+
+function prepare_plugins() {
+    echo "prepare plugins"
+    mv ${opengauss_source}/plugins/contrib/dolphin ${opengauss_source}/server/contrib/
+    cd ${opengauss_source}/server/contrib/dolphin
+    make write_git_commit git_repo_path=${opengauss_source}/plugins -k
+}
+
+function prepare_tools() {
+
+    # prepare 3rd
+    cd ${WORKSPACE};
+    
+    mkdir openGauss-third_party_binarylibs
+    tar -zxf openGauss-third_party_binarylibs.tar.gz -C openGauss-third_party_binarylibs --strip-components 1
+
+    os_plat=""
+    if [ "$(uname -p)" == "x86_64" ]; then
+        os_plat=openeuler_x86_64
+    else
+        os_plat=openeuler_aarch64
+    fi
+
+    # import gcc env
+    export os_platform=${os_plat}
+    export GCC_PATH=${WORKSPACE}/openGauss-third_party_binarylibs/buildtools/gcc7.3/
+    export CC=$GCC_PATH/gcc/bin/gcc
+    export CXX=$GCC_PATH/gcc/bin/g++
+    export LD_LIBRARY_PATH=$GCC_PATH/gcc/lib64:$GCC_PATH/isl/lib:$GCC_PATH/mpc/lib/:$GCC_PATH/mpfr/lib/:$GCC_PATH/gmp/lib/:$LD_LIBRARY_PATH
+    export PATH=$GCC_PATH/gcc/bin:$PATH
+
+    # we need cmake version >= 3.16
+    # cmake
+    cd ${WORKSPACE}/dependency
+    mkdir cmake
+    tar -xf cmake-3.19.5-Linux.tar.gz -C cmake --strip-components 1
+    export CMAKEHOME=${WORKSPACE}/dependency/cmake
+    export LD_LIBRARY_PATH=$CMAKEHOME/lib:$LD_LIBRARY_PATH
+    export PATH=${CMAKEHOME}/bin:${PATH}
+        
+}
+
+echo "build opengauss entry"
+
+start_time=$(date +'%F %H:%M:%S')
+
+download_source
+
+prepare_plugins
+
+prepare_tools
+
+echo "start to build opengass and package..."
+echo "sh -x package_ci_master.sh -d ${WORKSPACE} -t ${PKG_TYPE} -v ${PKG_VERSION} -b build -o ${OUT_PUT_PATH} -l ${WORKSPACE}/openGauss-third_party_binarylibs" > /home/aaa
+echo "sh -x package_ci_master.sh -d ${WORKSPACE} -t ${PKG_TYPE} -v ${PKG_VERSION} -b build -o ${OUT_PUT_PATH} -l ${WORKSPACE}/openGauss-third_party_binarylibs"
+sh package_ci_master.sh -d ${WORKSPACE} -t ${PKG_TYPE} -v ${PKG_VERSION} -b build -o ${OUT_PUT_PATH} -l ${WORKSPACE}/openGauss-third_party_binarylibs
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+mv ${OUT_PUT_PATH}/* ${VOLUME_PATH}/
+
+sh build_lite.sh
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+mv ${opengauss_source}/server_lite/output/* ${VOLUME_PATH}/
+
+end_time=$(date +'%F %H:%M:%S')
+echo "build opengauss end..."
+echo "build opengauss start time $start_time ."
+echo "build opengauss end time $end_time ."
